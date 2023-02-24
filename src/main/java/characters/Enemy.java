d7099ab71d738b3a2c56ec25528fd77fc16c37d9
DomJacobs00 <d.jokubauskas@edu.salford.ac.uk>
2023-02-24 20:03:25 +0000

    Updates

--- src/main/java/characters/Enemy.java
+++ src/main/java/characters/Enemy.java
@@ -1,5 +1,10 @@
 package characters;
 
-public class Enemy {
+public class Enemy extends Entity{
+
+	public Enemy(String name, double health, double x, double y, String appearance) {
+		super(name, health, x, y, appearance);
+		// TODO Auto-generated constructor stub
+	}
 
 }

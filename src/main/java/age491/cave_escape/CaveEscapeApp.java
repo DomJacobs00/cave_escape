d5cea48375135bab8f5edcf5b81ae59acb7a07ae
DomJacobs00 <d.jokubauskas@edu.salford.ac.uk>
2023-02-24 18:51:58 +0000

    Initial app.java creation
    Addition of the main class of the project: CaveEscapeApp.java

---
+++ src/main/java/age491/cave_escape/CaveEscapeApp.java
@@ -0,0 +1,34 @@
+package age491.cave_escape;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class CaveEscapeApp extends Application {
+	Pane root;
+	Scene scene;
+	Canvas canvas;
+	GraphicsContext gc;
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception
+	{
+		root = new Pane();
+		scene = new Scene(root,800,600);
+		canvas = new Canvas(800,600);
+		gc = canvas.getGraphicsContext2D();
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		root.getChildren().add(canvas);
+		gc.setFill(Color.BLACK);
+		gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+	}
+
+}

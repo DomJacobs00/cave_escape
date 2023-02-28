7e672c32f957881d5860760889f026ae64132f05
DomJacobs00 <d.jokubauskas@edu.salford.ac.uk>
2023-02-28 16:55:28 +0000

    Addition of multiple phases of character
    Movement images added

--- src/main/java/age491/cave_escape/Hero.java
+++ src/main/java/age491/cave_escape/Hero.java
@@ -4,7 +4,11 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
 public class Hero extends GameObject {
-
+	Image no_movement;
+	Image left_movement;
+	Image right_movement;
+	Image img;
+	
 	public Hero(double x, double y, GraphicsContext gc) {
 		super(x, y, gc);
 		img = new Image(Hero.class.getResource("test.png").toExternalForm());

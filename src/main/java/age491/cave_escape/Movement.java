79b895e36f9b1c445b6e5d6ebcc46cc80e354f5d
DomJacobs00 <d.jokubauskas@edu.salford.ac.uk>
2023-03-01 10:13:35 +0000

    Updating Movement.java

---
+++ src/main/java/age491/cave_escape/Movement.java
@@ -0,0 +1,26 @@
+package age491.cave_escape;
+
+public class Movement 
+{
+	double x=0, y=0;
+	double moveSpeed = 10.0;
+	double jumpSpeed = 13.0;
+	double gravity = 0.7;
+	double velocityY = 2.0;
+	public Movement() {
+		;
+	}
+	public void moveLeft()
+	{
+		
+	}
+	public void moveRight()
+	{
+		
+	}
+	public void jump()
+	{
+		
+	}
+	
+}

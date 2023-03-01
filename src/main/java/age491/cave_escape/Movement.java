e6e0b668e6116b11676138d09a176c3befe0e858
DomJacobs00 <d.jokubauskas@edu.salford.ac.uk>
2023-03-01 10:58:24 +0000

    Update Movement.java

--- src/main/java/age491/cave_escape/Movement.java
+++ src/main/java/age491/cave_escape/Movement.java
@@ -10,17 +10,54 @@ public class Movement
 	public Movement() {
 		;
 	}
-	public void moveLeft()
+	public double moveLeft(double inx)
+	{
+		x = inx - moveSpeed;
+		return x;
+	}
+	public double moveRight(double inx)
+	{
+		x = inx + moveSpeed;
+		return x;
+	}
+	public void jump(double iny)
 	{
 		
 	}
-	public void moveRight()
-	{
-		
-	}
-	public void jump()
-	{
-		
-	}
+	
+	boolean isAPressed = false;
+	boolean isDPressed = false;
+	/**
+	 * 
+	 
+	scene.setOnKeyPressed(event -> {
+	    if (event.getCode() == KeyCode.A) {
+	        isAPressed = true;
+	    }
+	    if (event.getCode() == KeyCode.D) {
+	        isDPressed = true;
+	    }
+	    
+	    if (isAPressed && isDPressed) {
+	        System.out.println("A and D are pressed together.");
+	    }
+	});
+
+	scene.setOnKeyReleased(event -> {
+	    if (event.getCode() == KeyCode.A) {
+	        isAPressed = false;
+	    }
+	    if (event.getCode() == KeyCode.D) {
+	        isDPressed = false;
+	    }
+	});
+	In this example, two boolean variables isAPressed and isDPressed are used to keep track of whether the "A" and "D" keys are currently pressed down. When a key is pressed, its corresponding boolean variable is set to true. When a key is released, its corresponding boolean variable is set to false.
+
+	The if (isAPressed && isDPressed) condition checks if both keys are currently pressed down, and if so, it prints a message to the console.
+	 */
+
+
+
+
 	
 }
